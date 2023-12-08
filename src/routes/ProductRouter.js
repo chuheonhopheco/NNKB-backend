3ed54fbbfdc3d325420a0fa5cf7520befdad2059
Ta3008 <nguyentuananh30082002@gmail.com>
2023-12-08 20:12:24 +0700

    het vid 25

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router()
 const ProductController = require('../controllers/ProductController');
-const { authMiddleWare } = require("../middleware/authMiddleware");
 
 router.post('/create',ProductController.createProduct) 
 router.put('/update/:id', ProductController.updateProduct)
--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -99,11 +99,38 @@ const getOneProduct = (id) => {
         }
     })
 }
-const getAllProduct = (limit = 8, page = 0) => {
+const getAllProduct = (limit, page, sort, filter) => {
     return new Promise(async(resolve, reject) => {
-        try{ 
-            const totalProduct = await Product.count()         
-            const allProduct = await Product.find().limit(limit).skip(page * limit)   
+        try{
+            const totalProduct = await Product.countDocuments()
+            if(filter){
+                const label = filter[0]
+                const allProductFilter = await Product.find({[label]: {'$regex' : filter[1]}}).limit(limit).skip(page * limit)
+                resolve({
+                    status: 'OK',
+                    message: 'SUCCESS',
+                    data: allProductFilter,
+                    total: totalProduct,
+                    pageCurrent: Number(page + 1),
+                    totalPage: Math.ceil(totalProduct / limit)
+                }) 
+            }
+            if(sort){
+                const objectSort = {}
+                objectSort[sort[1]] = sort[0]
+                const allProductSort = await Product.find().limit(limit).skip(page * limit).sort(objectSort)
+                resolve({
+                    status: 'OK',
+                    message: 'SUCCESS',
+                    data: allProductSort,
+                    total: totalProduct,
+                    pageCurrent: Number(page + 1),
+                    totalPage: Math.ceil(totalProduct / limit)
+                }) 
+            }
+            const allProduct = await Product.find().limit(limit).skip(page * limit).sort({
+                name: sort
+            })   
                 resolve({
                     status: 'OK',
                     message: 'SUCCESS',
