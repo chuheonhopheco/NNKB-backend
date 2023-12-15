6c2d37d9afb7cbb780d9cad338fb91b470a703d1
Ta3008 <nguyentuananh30082002@gmail.com>
2023-12-16 02:53:34 +0700

    loi 47

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ const port = process.env.PORT || 3001
 app.use(cors())
 app.use(bodyParser.json())
 app.use(cookieParser())
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({limit: '35mb'}));
+app.use(express.urlencoded({limit: '35mb'}));
 
 routes(app);
 
