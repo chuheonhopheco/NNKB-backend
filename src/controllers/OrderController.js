5ee46b0208c431f92071bb265e9c8e5b0b33b47b
chuheonhopheco <hdtung9a@gmail.com>
2023-12-17 10:24:11 +0700

    first commit

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -1,8 +1,6 @@
-const OrderSerice = require('../services/OrderService')
-
+const OrderService = require('../services/OrderService')
 const createOrder = async (req, res) => {
     try { 
-        console.log('req',req.body)
         const { paymentMethod, itemsPrice, shippingPrice, totalPrice, fullName, address, city, phone } = req.body
         if (!paymentMethod || !itemsPrice || !shippingPrice || !totalPrice || !fullName || !address || !city || !phone) {
             return res.status(200).json({
@@ -10,7 +8,7 @@ const createOrder = async (req, res) => {
                 message: 'The input is required'
             })
         }
-        const response = await OrderSerice.createOrder(req.body)
+        const response = await OrderService.createOrder(req.body)
         return res.status(200).json(response)
     } catch (e) {
         return res.status(404).json({
@@ -18,7 +16,64 @@ const createOrder = async (req, res) => {
         })
     }
 }
-
+const getAllOrderDetails = async (req, res) => {
+    try {
+        const userId = req.params.id
+        if (!userId) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The userId is required'
+            })
+        }
+        const response = await OrderService.getAllOrderDetails(userId)
+        return res.status(200).json(response)
+    } catch (e) {
+        console.log(e)
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+const getDetailsOrder = async (req, res) => {
+    try {
+        const orderId = req.params.id
+        if (!orderId) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The userId is required'
+            })
+        }
+        const response = await OrderService.getOrderDetails(orderId)
+        return res.status(200).json(response)
+    } catch (e) {
+        console.log(e)
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+const cancelOrderDetails = async (req, res) => {
+    try {
+        const orderId = req.params.id
+        const data = req.body
+        if (!orderId) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The userId is required'
+            })
+        }
+        const response = await OrderService.cancelOrderDetails(orderId, data)
+        return res.status(200).json(response)
+    } catch (e) {
+        console.log(e)
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
 module.exports = {
     createOrder,
+    getAllOrderDetails,
+    getDetailsOrder,
+    cancelOrderDetails
 }
--- src/routes/OrderRouter.js
+++ src/routes/OrderRouter.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const OrderController = require('../controllers/OrderController');
 
 router.post('/create', OrderController.createOrder)
-
+router.get('/get-all-order/:id', OrderController.getAllOrderDetails)
+router.get('/get-details-order/:id',OrderController.getDetailsOrder)
+router.delete('/cancel-order/:id', OrderController.cancelOrderDetails)
 module.exports = router
